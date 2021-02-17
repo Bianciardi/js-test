34b32cb46cb8935e508e53fc970208a9296b0dd5
Bianciardi <lorenzobianciardi@yahoo.it>
2021-02-17 16:07:03 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS</h1>`; */
 
-document.getElementById("identificatore").innerHTML = "<p>Contenuto</p>";
+document.getElementById("identificatore").innerHTML = "<p>Contenuto test</p>";
 
 console.log("Test");
 //window.alert("Test pop-up");
