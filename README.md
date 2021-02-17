6854d6d1fc3e8ce8fa84fd7f3849371e6c35dac6
Bianciardi <lorenzobianciardi@yahoo.it>
2021-02-17 16:00:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-wvuvjb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-wvuvjb)
---
+++ index.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+/* // Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS</h1>`; */
+
+document.getElementById("identificatore").innerHTML = "<p>Contenuto</p>";
+
+console.log("Test");
+//window.alert("Test pop-up");
